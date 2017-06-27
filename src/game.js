32c2a8e9791c76591e9d5a4383dd1f2e85559f5a
Todd Griffin <nemagnusfrater@gmail.com>
2017-06-27 11:10:31 -0400

    Adds additional functionality to game.js, updates game.js code docs.

--- src/game.js
+++ src/game.js
@@ -1,4 +1,44 @@
 /**
+ * Initializes the game.
+ *
+ * @method initGame
+ *
+ * @param {callback} tickCallback - method that updates all game data
+ * @param {callback} renderCallback - method that renders all game data
+ * @param {number} desired_ups - desired number of updates-per-second for both tick/render callbacks
+ *
+ * @return {object} returns an object containing a canvas and canvasContext
+ */
+function initGame (tickCallback, renderCallback, desired_ups) {
+	let canvasMaterials = createCanvas();
+
+	createGameLoop(tickCallback, renderCallback, desired_ups);
+
+	return canvasMaterials;
+}
+
+/**
+ * Creates a canvas object and canvasContext.
+ *
+ * @method createCanvas
+ *
+ * @return {object} returns an object containing a canvas and canvasContext.
+ */
+function createCanvas (){
+	// create canvas, get context
+	let canvas = document.createElement("canvas");
+	let canvasContext = canvas.getContext("2d");
+
+	// add canvas to body
+	document.body.appendChild(canvas);
+
+	return {
+		canvas,
+		canvasContext
+	}
+}
+
+/**
  * Creates game loop.
  *
  * @method createGameLoop
