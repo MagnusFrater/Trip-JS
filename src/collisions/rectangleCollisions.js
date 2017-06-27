6899f9fe90a76ef0416fd848fe7b450baaa5a588
Todd Griffin <nemagnusfrater@gmail.com>
2017-06-27 12:17:47 -0400

    Restructures rectangleCollisions.js to make sure it uses the new models and prototype functions of Rectangle.js.

--- src/collisions/rectangleCollisions.js
+++ src/collisions/rectangleCollisions.js
@@ -10,7 +10,7 @@
  * @return {boolean} true if collision imminent, false if otherwise
  */
 function rectCollideUp (rect1, rect2, step) {
-	return collideUp(rect1.y, rect2.bottom, step);
+	return collideUp(rect1.top(), rect2.bottom(), step);
 }
 
 /**
@@ -25,7 +25,7 @@ function rectCollideUp (rect1, rect2, step) {
  * @return {boolean} true if collision imminent, false if otherwise
  */
 function rectCollideDown (rect1, rect2, step) {
-	return collideDown(rect1.bottom, rect2.y, step);
+	return collideDown(rect1.bottom(), rect2.top(), step);
 }
 
 /**
@@ -40,7 +40,7 @@ function rectCollideDown (rect1, rect2, step) {
  * @return {boolean} true if collision imminent, false if otherwise
  */
 function rectCollideLeft (rect1, rect2, step) {
-	return collideLeft(rect1.x, rect2.right, step);
+	return collideLeft(rect1.left(), rect2.right(), step);
 }
 
 /**
@@ -55,7 +55,7 @@ function rectCollideLeft (rect1, rect2, step) {
  * @return {boolean} true if collision imminent, false if otherwise
  */
 function rectCollideRight (rect1, rect2, step) {
-	return collideRight(rect1.right, rect2.x, step);
+	return collideRight(rect1.right(), rect2.left(), step);
 }
 
 /**
